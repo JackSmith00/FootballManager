bd17d327ae817d3b770818c7eefd74c4655b2378
Jack-24265241 <24265241@edgehill.ac.uk>
2021-12-06 21:09:43 +0000

    Result Output almost done
    The result output is lay out correctly, it just has an issue with the year being displayed being wrong

--- FootballManagerApp/src/guiOutputDialogs/ResultOutputDialog.java
+++ FootballManagerApp/src/guiOutputDialogs/ResultOutputDialog.java
@@ -4,10 +4,12 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import enums.EventType;
 
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -29,9 +31,11 @@ public class ResultOutputDialog extends JDialog {
 		super(owner, result.getHomeTeam().getTeamName() + " v " + result.getAwayTeam().getTeamName());
 		this.result = result;
 		
+		System.out.println(result.getDatePlayed());
 		setUpComponents();
 		getContentPane().add(frame);
-		setBounds(0, 0, 300, 300);
+		setBounds(0, 0, 500, 450);
+		setResizable(false);
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
@@ -39,8 +43,8 @@ public class ResultOutputDialog extends JDialog {
 	protected void setUpComponents() {
 		frame = new JPanel();
 		GridBagLayout layout = new GridBagLayout();
-		layout.columnWidths = new int[] {300, 50, 300};
-		layout.rowHeights = new int[] {25, 25, 25, 150, 25, 200};
+		layout.columnWidths = new int[] {200, 50, 200};
+		layout.rowHeights = new int[] {25, 25, 25, 150, 25, 150};
 		frame.setLayout(layout);
 		
 		GridBagConstraints constraints = new GridBagConstraints();
@@ -86,26 +90,30 @@ public class ResultOutputDialog extends JDialog {
 		String[] playerTableColumnNames = {"Name", "Position"};
 		Player[] homePlayers = result.getHomePlayers();
 		UneditableTableWithRowObjectReturn homePlayerTable = new UneditableTableWithRowObjectReturn(generatePlayerTableContents(homePlayers), playerTableColumnNames, homePlayers);
+		JScrollPane homePlayerTableScrollPane = new JScrollPane(homePlayerTable);
 		DateFormat dateFormatter = DateFormat.getDateInstance(); // https://docs.oracle.com/javase/7/docs/api/java/text/DateFormat.html
 		JLabel date = new JLabel(dateFormatter.format(result.getDatePlayed()));
 		Player[] awayPlayers = result.getAwayPlayers();
 		UneditableTableWithRowObjectReturn awayPlayerTable = new UneditableTableWithRowObjectReturn(generatePlayerTableContents(awayPlayers), playerTableColumnNames, awayPlayers);
-		
-		constraints.gridx = 0;
-		constraints.gridy = 3;
-		frame.add(homePlayerTable, constraints);
+		JScrollPane awayPlayerTableScrollPane = new JScrollPane(awayPlayerTable);
 		
 		constraints.anchor = GridBagConstraints.NORTH;
-		constraints.gridx = 2;
+		constraints.gridx = 1;
+		constraints.gridy = 3;
+		constraints.insets = new Insets(0, 15, 0, 15);
 		frame.add(date, constraints);
 		
 		constraints.anchor = GridBagConstraints.CENTER;
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.gridx = 0;
+		frame.add(homePlayerTableScrollPane, constraints);
+		
 		constraints.gridx = 2;
-		frame.add(awayPlayerTable, constraints);
+		frame.add(awayPlayerTableScrollPane, constraints);
 		
 		JLabel eventsLabel = new JLabel("Events");
+		eventsLabel.setHorizontalAlignment(JLabel.CENTER);
 		
-		constraints.fill = GridBagConstraints.BOTH;
 		constraints.gridx = 0;
 		constraints.gridwidth = 3;
 		constraints.gridy = 4;
@@ -113,9 +121,10 @@ public class ResultOutputDialog extends JDialog {
 		
 		GameEvent[] events = result.getEvents();
 		UneditableTableWithRowObjectReturn eventsTable = new UneditableTableWithRowObjectReturn(generateEventsTableContents(events), new String[] {"Type", "Player(s)", "Time"}, events);
+		JScrollPane eventsTableScrollPane = new JScrollPane(eventsTable);
 		
 		constraints.gridy = 5;
-		frame.add(eventsTable, constraints);
+		frame.add(eventsTableScrollPane, constraints);
 	}
 	
 	private Object[][] generatePlayerTableContents(Player[] players) {
